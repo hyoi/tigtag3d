bbdb9da2e14243a5fa234eebb1c12d772a55266d
hyoi <hyoi.kaz@gmail.com>
2025-10-21 07:38:18 +0900

    3DゲームのMyState::StageStartとMyState::MainLoopを有効化。不要なコメントを削除。

--- crates/tigtag3d/src/core_logic/schedule.rs
+++ crates/tigtag3d/src/core_logic/schedule.rs
@@ -11,29 +11,8 @@ impl Plugin for Schedule
         //--------------------------------------------------------------------------
         // 各種登録
         application
-            // スケジュールの追加
-            // .add_plugins(init_app::Schedule { next: MyState::Initialize } ) // アプリ初期化とアセットロード
-            // .add_plugins(demo_play::Schedule)                               // デモプレイ
-            // .add_plugins(overlay_ui::pause_menu::Schedule)                  // Pauseメニュー
-
             // Resourceの登録
             .init_resource::<map::Dots3D>() //3DドットEntityの保存用2次元vec
-
-            // .init_resource::<CameraSettings>()                  // カメラの設定を登録
-            // .init_resource::<Record>()                          // ゲームの成績
-            // .init_resource::<misc::MaskHitAnyKeyInput>()        // 「Hit Any Key」の入力マスク
-            // .init_resource::<map::Map>()                        // ステージのマップ
-            // .insert_resource(handle_input::MappingKeyboard::from(KEYBOARD_MAP)) // マッピング
-            // .insert_resource(handle_input::MappingGamepad::from(GAMEPAD_MAP))   // マッピング
-
-            // Messageの登録
-            // .add_message::<misc::AnyButtonPressed>() //「Hit Any Key」の入力通知
-            // .add_message::<SkipOverlayMessage>()     // 全画面メッセージ表示のスキップに使用
-            // .add_message::<CountDownEnded>()         // カウントダウンの終了通知
-            // .add_message::<handle_input::MessUserAction>() // デバイスからの入力
-            // .add_message::<DotsAllEaten >()          // ステージクリアの伝達用
-            // .add_message::<DotEaten>()               // スコアリングの伝達用
-            // .add_message::<PlayerCaught>()           // ゲームオーバーの伝達用
             ;
 
         //--------------------------------------------------------------------------
@@ -51,23 +30,6 @@ impl Plugin for Schedule
                     change_camera3d_settings,
                 ),
             );
-
-        //--------------------------------------------------------------------------
-        // 常に実行する処理（Update without MyState）
-        // application
-        //     // ループ処理
-        //     .add_systems(
-        //         Update, // without MyState
-        //         (
-        //             // ヘッダーとフッターの表示情報を更新する
-        //             information::update_header_footer,
-        //             // スプライトアニメーション
-        //             animate_sprites::<player::Player>, // プレイヤー
-        //             animate_sprites::<chaser::Chaser>, // チェイサー
-        //             // スプライト表示OFFの場合のアニメーション
-        //             chaser::rotate_chaser_shape.run_if(SPRITE_OFF), // チェイサー回転
-        //         ),
-        //     );
 
         //--------------------------------------------------------------------------
         // タイトル画面の処理（MyState::TitleDemo）
@@ -107,163 +69,43 @@ impl Plugin for Schedule
 
         //--------------------------------------------------------------------------
         // ゲーム開始処理（MyState::StageStart）
-        // application
-        //     // 前処理
-        //     .add_systems(
-        //         OnEnter(MyState::StageStart),
-        //         (
-        //             // 全画面メッセージの表示スキップ指示があるなら即メインループへ
-        //             misc::set_next_state(MyState::MainLoop)
-        //                 .run_if(on_message::<SkipOverlayMessage>),
-        //             // 全画面メッセージ（ステージ開始）表示
-        //             (
-        //                 OverlayStageStart::init(),
-        //                 misc::show_component::<OverlayStageStart>
-        //                     .after(OverlayStageStart::init()),
-        //             )
-        //                 .run_if(not(on_message::<SkipOverlayMessage>)),
-        //             // ステージ初期化
-        //             map::make_new_stage_data, // マップデータ
-        //             (
-        //                 map::spawn_sprite,    // マップスプライト
-        //                 player::spawn_sprite, // プレーヤースプライト
-        //                 chaser::spawn_sprite, // チェイサースプライト
-        //             )
-        //                 .after(map::make_new_stage_data),
-        //         ),
-        //     )
-        //     // ループ処理
-        //     .add_systems(
-        //         Update, // within MyState::StageStart
-        //         (
-        //             // カウントダウン完了後にState遷移
-        //             overlay_ui::effect::countdown::<OverlayStageStart>,
-        //             misc::set_next_state(MyState::MainLoop)
-        //                 .run_if(on_message::<CountDownEnded>)
-        //                 .after(overlay_ui::effect::countdown::<OverlayStageStart>),
-        //         )
-        //             .run_if(in_state(MyState::StageStart)),
-        //     )
-        //     // 後処理
-        //     .add_systems(
-        //         OnExit(MyState::StageStart),
-        //         (
-        //             // 全画面メッセージ（ステージ開始）非表示
-        //             misc::hide_component::<OverlayStageStart>,
-        //         ),
-        //     );
+        application
+            // 前処理
+            .add_systems(
+                OnEnter(MyState::StageStart),
+                (
+                    // 3Dのマップをspawnする
+                    map::spawn_3d_map_entity
+                        .after(tigtag2d::core_logic::map::make_new_stage_data),
+                    // 3Dのプレイヤーをspawnする
+                    player::spawn_3d_player
+                        .after(tigtag2d::core_logic::player::spawn_sprite),
+                    // 3Dのチェイサーをspawnする
+                    chaser::spawn_3d_chasers
+                        .after(tigtag2d::core_logic::chaser::spawn_sprite),
+                    // ミニマップカメラをspawnする(2Dプレイヤーの子にする)
+                    player::spawn_minimap_camera
+                        .after(tigtag2d::core_logic::player::spawn_sprite),
+                ),
+            );
 
         // --------------------------------------------------------------------------
         // メインループ処理（MyState::MainLoop）
-        // application
-        //     // ループ処理
-        //     .add_systems(
-        //         Update, // within MyState::MainLoop
-        //         (
-        //             (
-        //                 // スプライトの位置を更新する
-        //                 handle_input::check_keyboard, // キー
-        //                 handle_input::check_gamepad,  // ゲームパッド
-        //                 player::move_sprite
-        //                     .after(handle_input::check_keyboard)
-        //                     .after(handle_input::check_gamepad)
-        //                     .run_if(on_message::<handle_input::MessUserAction>),
-        //                 chaser::move_sprite,
-        //             ),
-        //             // スコアリング＆クリア判定
-        //             detecting_change::scoring_and_stage_clear,
-        //             misc::set_next_state(MyState::StageClear)
-        //                 .run_if(on_message::<DotsAllEaten>),
-        //             // 衝突判定
-        //             detecting_change::collisions_and_gameover
-        //                 // DotsAllEaten ➡ スキップ
-        //                 .run_if(not(on_message::<DotsAllEaten>)),
-        //             misc::set_next_state(MyState::GameOver)
-        //                 .run_if(on_message::<PlayerCaught>),
-        //         )
-        //             .chain()
-        //             .run_if(in_state(MyState::MainLoop)),
-        //     );
-
-        //--------------------------------------------------------------------------
-        // ステージクリアの処理（MyState::StageClear）
-        // application
-        //     // 前処理
-        //     .add_systems(
-        //         OnEnter(MyState::StageClear),
-        //         (
-        //             // 全画面メッセージ（ステージクリア）表示
-        //             OverlayStageClear::init(),
-        //             misc::show_component::<OverlayStageClear>
-        //                 .after(OverlayStageClear::init()),
-        //         ),
-        //     )
-        //     // ループ処理
-        //     .add_systems(
-        //         Update, // within MyState::StageClear
-        //         (
-        //             // カウントダウン完了後にState遷移
-        //             overlay_ui::effect::countdown::<OverlayStageClear>,
-        //             misc::set_next_state(MyState::StageStart)
-        //                 .run_if(on_message::<CountDownEnded>)
-        //                 .after(overlay_ui::effect::countdown::<OverlayStageClear>),
-        //         )
-        //             .run_if(in_state(MyState::StageClear)),
-        //     )
-        //     // 後処理
-        //     .add_systems(
-        //         OnExit(MyState::StageClear),
-        //         (
-        //             // 全画面メッセージ（ステージクリア）非表示
-        //             misc::hide_component::<OverlayStageClear>,
-        //             // 後続の MyState::StageStart で全画面メッセージを表示しない
-        //             misc::set_message::<SkipOverlayMessage>,
-        //         ),
-        //     );
-
-        //--------------------------------------------------------------------------
-        // ゲームオーバーの処理（MyState::GameOver）
-        // application
-        //     // 前処理
-        //     .add_systems(
-        //         OnEnter(MyState::GameOver),
-        //         (
-        //             // 全画面メッセージ（ステージクリア）表示
-        //             OverlayGameOver::init(),
-        //             misc::show_component::<OverlayGameOver>
-        //                 .after(OverlayGameOver::init()),
-        //         ),
-        //     )
-        //     // ループ処理
-        //     .add_systems(
-        //         Update, // within MyState::GameOver
-        //         (
-        //             // カウントダウン完了後にState遷移
-        //             overlay_ui::effect::countdown::<OverlayGameOver>,
-        //             misc::set_next_state(MyState::TitleDemo)
-        //                 .run_if(on_message::<CountDownEnded>)
-        //                 .after(overlay_ui::effect::countdown::<OverlayGameOver>),
-        //             // Replay? の明滅
-        //             overlay_ui::effect::blinking_text::<OverlayGameOver>,
-        //             // Hit ANY Key に反応あればState遷移
-        //             misc::check_hit_any_key
-        //                 .in_set(misc::execution_order::Target::HitAnyKey),
-        //             misc::set_next_state(MyState::StageStart)
-        //                 .in_set(misc::execution_order::After::HitAnyKey)
-        //                 .run_if(on_message::<misc::AnyButtonPressed>),
-        //         )
-        //             .run_if(in_state(MyState::GameOver)),
-        //     )
-        //     // 後処理
-        //     .add_systems(
-        //         OnExit(MyState::GameOver),
-        //         (
-        //             // 全画面メッセージ（ステージクリア）非表示
-        //             misc::hide_component::<OverlayGameOver>,
-        //             // scoreとstageをゼロクリアする
-        //             detecting_change::initialize_score_stage,
-        //         ),
-        //     );
+        application
+            // ループ処理
+            .add_systems(
+                Update,
+                (
+                    // 3Dマップのドットを消す処理
+                    map::update_3d_map
+                        .run_if(on_message::<tigtag2d::core_logic::DotEaten>),
+                    // 3Dプレイヤーの移動
+                    player::update_3d_player,
+                    // 3Dチェイサーの移動
+                    chaser::update_3d_chasers,
+                )
+                    .run_if(in_state(MyState::MainLoop)),
+            );
     }
 }
 
